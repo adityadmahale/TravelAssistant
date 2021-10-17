6ee777f0ebce8789b1e04573d1c4123959c8f5bf
Aditya Mahale <aditya.m258@gmail.com>
2021-10-17 14:12:30 +0530

    Add TravelOption class. The class will act as an edge between two vertices

---
+++ src/TravelOption.java
@@ -0,0 +1,38 @@
+
+public class TravelOption {
+    City startCity;
+    City destinationCity;
+    private String mode;
+    private float cost;
+    private int duration;
+    
+    public TravelOption(City startCity, City destinationCity, String mode, float cost,
+	    int duration) {
+	this.startCity = startCity;
+	this.destinationCity = destinationCity;
+	this.mode = mode;
+	this.cost = cost;
+	this.duration = duration;
+    }
+
+    public City getStartCity() {
+        return startCity;
+    }
+
+    public City getDestinationCity() {
+        return destinationCity;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+    
+}
