353f414a0fe527e3c999a65f4938bcacf93855b0
Aditya Mahale <aditya.m258@gmail.com>
2021-10-17 13:19:14 +0530

    Define a map to keep track of the city name and it corresponding object

--- src/TravelAssistant.java
+++ src/TravelAssistant.java
@@ -1,10 +1,26 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TravelAssistant {
     
+    // Stores the city name and its corresponding City object.
+    Map<String, City> cities = new HashMap<>();
+    
     public boolean addCity( String cityName, boolean testRequired, int timeToTest,
 	    int nightlyHotelCost) throws IllegalArgumentException {
-	return false;
+	// If the city is already known by the TravelAssistant, then return false
+	if (cities.containsKey(cityName)) return false;
+	
+	// Throw an exception when then the hotel cost is invalid
+	if (nightlyHotelCost < 0) throw new IllegalArgumentException();
+	
+	// At this point, the passed arguments are correct.
+	// Create a new City object and add it to the cities Map.
+	var city = new City(cityName, testRequired, timeToTest, nightlyHotelCost);
+	cities.put(cityName, city);
+	
+	return true;
     }
     
     public boolean addFlight( String startCity, String destinationCity, int flightTime,
