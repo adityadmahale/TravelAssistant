04a0b42bd56c0b018340795551a3cf9854036f32
Aditya Mahale <aditya.m258@gmail.com>
2021-10-17 12:22:59 +0530

    Create method stubs in the TravelAssistant class

---
+++ src/TravelAssistant.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class TravelAssistant {
+    
+    public boolean addCity( String cityName, boolean testRequired, int timeToTest,
+	    int nightlyHotelCost) throws IllegalArgumentException {
+	return false;
+    }
+    
+    public boolean addFlight( String startCity, String destinationCity, int flightTime,
+	    int flightCost) throws IllegalArgumentException {
+		return false;
+    }
+    
+    boolean addTrain( String startCity, String destinationCity, int trainTime, int trainCost)
+	    throws IllegalArgumentException {
+		return false;
+    }
+    
+    public List<String> planTrip ( String startCity, String destinationCity, boolean isVaccinated, int
+	    costImportance, int travelTimeImportance, int travelHopImportance ) throws
+	    IllegalArgumentException {
+	return null;
+    }
+}
