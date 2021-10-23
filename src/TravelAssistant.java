d6cf989cd5c60e2ef6c92106b46a2d6b69b8648b
Aditya Mahale <aditya.m258@gmail.com>
2021-10-23 16:48:43 +0530

    Extract methods for the city nodes validation

--- src/TravelAssistant.java
+++ src/TravelAssistant.java
@@ -38,21 +38,27 @@ public class TravelAssistant {
 	return addTravelMode(startCity, destinationCity, trainTime, trainCost, "train");
     }
     
+    private void areNodesCorrect(City fromCity, City toCity) throws IllegalArgumentException {
+	// Throw an exception if:
+	// The start and destination are the same city.
+	// The start and destination cities are not present
+	if (fromCity == null || toCity == null || fromCity == toCity) {
+	    throw new IllegalArgumentException();
+	}
+    }
+    
     private boolean addTravelMode(String startCity, String destinationCity, int duration,
 	    int cost, String mode) throws IllegalArgumentException {
 	// Check if the cost and duration are negative values
 	if (duration < 0 || cost < 0) throw new IllegalArgumentException();
 	
-	// Get the start city object. Throw an exception if the city is not present.
+	// Get the start city and destination city objects
 	var fromCity = cities.get(startCity);
-	if (fromCity == null) throw new IllegalArgumentException();
-				
-	// Get the destination city object. Throw an exception if the city is not present.
 	var toCity = cities.get(destinationCity);
-	if (toCity == null) throw new IllegalArgumentException();
+	areNodesCorrect(fromCity, toCity);
 	
-	// Check if the inputs are correct
-	if (!isTravelInputsCorrect(fromCity, toCity, duration, cost, mode)) return false;
+	// Check if the travel mode is already present
+	if (isTravelModePresent(fromCity, toCity, mode)) return false;
 	
 	// At this point, the inputs are correct. So, a new edge can be added between the two cities.
 	adjacencyList.get(fromCity).add(new TravelHop(fromCity, toCity, mode, cost, duration));
@@ -60,21 +66,18 @@ public class TravelAssistant {
 	return true;
     }
     
-    private boolean isTravelInputsCorrect(City fromCity, City toCity, int duration,
-	    int cost, String mode) throws IllegalArgumentException {	
-	// Throw an exception if the start and destination are the same city.
-	if (fromCity == toCity) throw new IllegalArgumentException();
-	
+    private boolean isTravelModePresent(City fromCity, City toCity, String mode) 
+	    throws IllegalArgumentException {		
 	// Handle the case when the edge is already present.
 	String destinationCity;
 	String modeOfTravel;
 	for (var travelOption: adjacencyList.get(fromCity)) {
 	    destinationCity = travelOption.getDestinationCity().getCityName();
 	    modeOfTravel = travelOption.getMode();
-	    if (destinationCity.equals(toCity.getCityName()) && modeOfTravel.equals(mode)) return false;
+	    if (destinationCity.equals(toCity.getCityName()) && modeOfTravel.equals(mode)) return true;
 	}
 	
-	return true;
+	return false;
     }
     
     public void print() {
@@ -88,6 +91,7 @@ public class TravelAssistant {
     public List<String> planTrip ( String startCity, String destinationCity, boolean isVaccinated, int
 	    costImportance, int travelTimeImportance, int travelHopImportance ) throws
 	    IllegalArgumentException {
+	
 	return null;
     }
     
